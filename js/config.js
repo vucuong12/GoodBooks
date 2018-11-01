74b9a13865a1a67487077810739736ff4ff66a91
vucuong12 <vucuong020195@gmail.com>
2018-11-01 19:45:10 +0900

    add skeleton for userpage and database

---
+++ js/config.js
@@ -0,0 +1,13 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAzAIcCWeoIna96vmvoz0lYbIoOf74SH7A",
+  authDomain: "goodbooks-619e8.firebaseapp.com",
+  databaseURL: "https://goodbooks-619e8.firebaseio.com",
+  projectId: "goodbooks-619e8",
+  storageBucket: "goodbooks-619e8.appspot.com",
+  messagingSenderId: "203399060877"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
+var booksDb = database.ref("testitems");
